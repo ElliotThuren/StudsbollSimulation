6a8ac23b3cea82f01f831a2f580f23b025790251
ElliotThuren <elliot.thuren@elev.ga.ntig.se>
2021-09-06 09:35:21 +0200

    6/9-2021

--- StudsandeBoll.java
+++ StudsandeBoll.java
@@ -1,5 +1,27 @@
 public class StudsandeBoll {
-    public static void main(String[] args) {
+    public double velocity = 0;
+    public double height = 10;
+    public double acceleration = -9.82;
 
+    public void updateVelocity() {
+        velocity = velocity + acceleration * 0.1;
+    }
+
+    public void updateHeight() {
+        height = height + velocity * 0.1;
+    }
+
+    public String ballPosition() {
+        updateVelocity();
+        updateHeight();
+
+        return velocity + " : " + height;
+    }
+
+    public static void main(String[] args) {
+        StudsandeBoll studs = new StudsandeBoll();
+        while(studs.ballPosition() != null) {
+            System.out.println(studs.ballPosition());
+        }
     }
 }
